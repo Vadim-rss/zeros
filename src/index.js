9f412ad0c0f51acb81f88084ebcc9d5744325c45
Vadim-rss <55102312+Vadim-rss@users.noreply.github.com>
2019-10-11 21:00:48 +0300

    feat: implement function zeros(str)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let array = expression.split('*');
+  let num;
+  let count_num;
+  
+  let fact; 
+  let result = 0;
+  let sumTwo = 0; 
+  let sumFive = 0;
+
+  for (let i = 0; i < array.length; i++) {
+
+    if (array[i].search('!!') > 0) {
+      num = array[i].replace('!!', '');
+      fact = 2;
+    } else {
+      num = array[i].replace('!', '');
+      fact = 1;
+    }
+
+    while (num != 0 && num != 1) {
+      count_num = num;
+
+      while (count_num % 2 === 0) {
+       sumTwo++;
+       count_num = count_num / 2;
+      }
+
+      while (count_num % 5 === 0) {
+       sumFive++;
+       count_num = count_num / 5;
+      }
+      num = num - fact;
+    }
+  }
+  result = Math.min(sumTwo, sumFive);
+  return result
 }
